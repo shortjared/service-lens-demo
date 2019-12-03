0d1b270d07153b535de40bf1eef0412fafe8c8ca
EC2 Default User <ec2-user@ip-172-31-95-205.ec2.internal>
2019-12-03 19:11:34 +0000

    updated info

---
+++ src/handlers/get-all-items.js
@@ -0,0 +1,42 @@
+// Create clients and set shared const values outside of the handler.
+
+// Get the DynamoDB table name from environment variables
+const tableName = process.env.SAMPLE_TABLE;
+const AWSXRay = require('aws-xray-sdk');
+
+// Create a DocumentClient that represents the query to add an item
+const AWS = require('aws-sdk');
+const docClient = new AWS.DynamoDB.DocumentClient({
+  service: new AWS.DynamoDB()
+});
+
+AWSXRay.captureAWSClient(docClient.service);
+
+/**
+ * A simple example includes a HTTP get method to get all items from a DynamoDB table.
+ */
+exports.getAllItemsHandler = async (event) => {
+    if (event.httpMethod !== 'GET') {
+        throw new Error(`getAllItems only accept GET method, you tried: ${event.httpMethod}`);
+    }
+    // All log statements are written to CloudWatch
+    console.info('received:', event);
+
+    // get all items from the table (only first 1MB data, you can use `LastEvaluatedKey` to get the rest of data)
+    // https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/DynamoDB/DocumentClient.html#scan-property
+    // https://docs.aws.amazon.com/amazondynamodb/latest/APIReference/API_Scan.html
+    var params = {
+        TableName : tableName
+    };
+    const data = await docClient.scan(params).promise();
+    const items = data.Items;
+
+    const response = {
+        statusCode: 200,
+        body: JSON.stringify(items)
+    };
+
+    // All log statements are written to CloudWatch
+    console.info(`response from: ${event.path} statusCode: ${response.statusCode} body: ${response.body}`);
+    return response;
+}
